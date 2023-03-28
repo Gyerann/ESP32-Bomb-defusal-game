491b0fb3501f918eed57cee788003c7894c2c38c
Gyerann <sandor.selyem01@gmail.com>
2023-03-29 00:29:51 +0200

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "tm1637.h"
+
+//TM1637 pins
+#define TM1637_CLK_PIN GPIO_NUM_18
+#define TM1637_DIO_PIN GPIO_NUM_19
+#define BEEP_PIN GPIO_NUM_13
+#define STRIKE_1_PIN GPIO_NUM_4
+#define STRIKE_2_PIN GPIO_NUM_32
+#define STRIKE_3_PIN GPIO_NUM_2
+
+#define STRIKE_BTN GPIO_NUM_33
+
+#define SOUND_ON false
+
+void beep() {
+    if(SOUND_ON){
+        gpio_set_level(BEEP_PIN, 1);
+        vTaskDelay(125 / portTICK_PERIOD_MS);
+        gpio_set_level(BEEP_PIN, 0);
+    } else {
+        vTaskDelay(125 / portTICK_PERIOD_MS);
+    }  
+}
+
+void update_timer(tm1637_led_t* display, int *currTime) {
+    *currTime = *currTime - 1;
+    tm1637_set_number_lead_dot(display, *currTime, 0, 0b11111111);
+    beep();
+    vTaskDelay(875 / portTICK_PERIOD_MS);
+}
+
+//Initializing GPIO pins
+void init_gpio_pins(){
+    esp_err_t beepPinDir = gpio_set_direction(BEEP_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(BEEP_PIN, 0);
+
+    esp_err_t strike1Pin = gpio_set_direction(STRIKE_1_PIN, GPIO_MODE_OUTPUT);
+    esp_err_t strike2Pin = gpio_set_direction(STRIKE_2_PIN, GPIO_MODE_OUTPUT);
+    esp_err_t strike3Pin = gpio_set_direction(STRIKE_3_PIN, GPIO_MODE_OUTPUT);
+    gpio_set_level(STRIKE_1_PIN, 0);
+    gpio_set_level(STRIKE_2_PIN, 0);
+    gpio_set_level(STRIKE_3_PIN, 0);
+
+    esp_err_t strikeBtnPin = gpio_set_direction(STRIKE_BTN, GPIO_MODE_INPUT);
+    gpio_set_level(STRIKE_BTN, 0);
+}
+
+void add_strike(int *currStrikes){
+*currStrikes = *currStrikes + 1;
+switch(*currStrikes) {
+    case 1:
+        gpio_set_level(STRIKE_1_PIN, 1);
+        break;
+    case 2:
+        gpio_set_level(STRIKE_2_PIN, 1);
+        break;
+    case 3:
+        gpio_set_level(STRIKE_3_PIN, 1);
+        break;
+}
+}
+
+void app_main() {
+
+init_gpio_pins();
+
+//Display setup
+tm1637_led_t* tm1637 = tm1637_init(TM1637_CLK_PIN, TM1637_DIO_PIN);
+tm1637_set_brightness(tm1637, 4);
+
+//Variables
+int secondsLeft = 30;
+int maxStrikes = 3;
+int currStrikes = 0;
+
+bool solved = false;
+
+    while (secondsLeft != 0 && !solved) {
+        update_timer(tm1637, &secondsLeft);
+
+        if(gpio_get_level(STRIKE_BTN) == 1){
+            add_strike(&currStrikes);
+        }
+    }
+}
