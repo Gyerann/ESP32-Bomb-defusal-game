536280a076d688dc5b15e3db6e732f2866f20b06
Gyerann <107305694+Gyerann@users.noreply.github.com>
2023-09-03 20:04:17 +0200

    Create test_cut_the_wire.cpp
    +Test case

---
+++ test/test_cut_the_wire/test_cut_the_wire.cpp
@@ -0,0 +1,42 @@
+#include <unity.h>
+#include "player.h"
+
+#define TEST_TAG "test_cut_the_wire_module"
+
+void setUp(){
+
+}
+
+void tearDown(){
+
+}
+
+void test_solution_generated(){
+
+}
+
+void test_current_solution_matching_generated_solution(){
+
+}
+
+void test_check_module(){
+
+}
+
+void test_solve_module(){
+
+}
+
+void test_get_module_state(){
+    
+}
+
+extern "C" int app_main(int argc, char** argv){
+    UNITY_BEGIN();
+
+    
+
+    UNITY_END();
+
+    return 0;
+}
