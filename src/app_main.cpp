aa2736e1590308704e38d82d021d05c647813b08
Gyerann <107305694+Gyerann@users.noreply.github.com>
2023-09-23 21:44:08 +0200

    Update app_main.cpp

--- src/app_main.cpp
+++ src/app_main.cpp
@@ -22,15 +22,21 @@ void network_init(){
     setup_server();
 }
 
-extern "C" void app_main() {
-
-	esp_err_t ret = nvs_flash_init();
+esp_err_t memory_init(){
+    esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
     {
         ESP_ERROR_CHECK(nvs_flash_erase());
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
+    
+    return ret;
+}
+
+extern "C" void app_main() {
+
+	esp_err_t ret = memory_init();
 
     uint8_t ctwSeed = rand();
     CutTheWireModule cutTheWireModule(ctwSeed);
